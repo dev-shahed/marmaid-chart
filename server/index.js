54c543a263612d9173bdc2e9d68ecdaaa79b44d5
dev-shahed <shahedthedev@gmail.com>
2024-10-05 01:55:34 +0600

    update vercel.json

--- server/index.js
+++ server/index.js
@@ -2,8 +2,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const path = require('path')
-const chartRoutes = require('./routes/chartRoutes'); 
+const path = require('path');
+const chartRoutes = require('./routes/chartRoutes');
 dotenv.config();
 
 // Create an Express app
@@ -12,18 +12,15 @@ app.use(express.json());
 app.use(cors());
 
 // Serve the static files from the React app
-app.use(express.static(path.join(__dirname, 'dist')))
+app.use(express.static(path.join(__dirname, 'dist')));
 
-// Use the chart routes
+
 app.use('/api', chartRoutes);
-
 
 // Serve the application frontend..
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
-})
-
-
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 // Start the Express server
 const PORT = process.env.PORT || 5000;
